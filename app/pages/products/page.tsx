d1586518e0ca04a5e77ad7e85e93fb41ad9005f9
Consolider <christian.spector@protonmail.com>
2025-09-15 13:58:49 +0200

    Created component and added it to products page

--- app/pages/products/page.tsx
+++ app/pages/products/page.tsx
@@ -1,8 +1,10 @@
+import SearchBarProducts from "@/components/search-bar-products"
 import FeaturedProductList from "@/components/featured-product-list"
 
 export default function Products() {
     return (
         <div>
+            <SearchBarProducts />
             <FeaturedProductList />
         </div>
     )
--- components/search-bar-products.tsx
+++ components/search-bar-products.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from "use-debounce";
 
-export default function SearchBarRounded() {
+export default function SearchBarProducts() {
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const { replace } = useRouter();
@@ -12,32 +12,34 @@ export default function SearchBarRounded() {
     const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
     if (term) {
-      params.set('query', term);
+      params.set('search', term);
     } else {
-      params.delete('query');
+      params.delete('search');
     }
     replace(`${pathname}?${params.toString()}`);
     }, 600)
 
     return (
-        <div className="relative inline-block">
+        <section className="flex justify-center">
+        <div className="relative inline-block w-6/10">
             <input
                 type="text"
-                aria-label="Search for a pokèmon"
-                placeholder="Search for a pokèmon..."
-                className="py-2 px-4 w-[400px] border"
+                aria-label="Search for a product"
+                placeholder="Search for a product..."
+                className="py-2 px-4 w-full border rounded-full"
                 onChange={(e) => {
                     handleSearch(e.target.value);
                 }}
-            defaultValue={searchParams.get('query')?.toString()}
+            defaultValue={searchParams.get('search')?.toString()}
             />
             <Image
-                src="/Search.svg"
+                src="/search.svg"
                 alt="Search icon"
                 width={50}
                 height={50}
-                className="absolute dark:invert opacity-25 mt-[3px] right-4 top-1/2 transform -translate-y-1/2 w-5 h-5 pointer-events-none"
+                className="absolute opacity-25 right-4 top-1/2 transform -translate-y-1/2 w-5 h-5 pointer-events-none"
             />
         </div>
+        </section>
     )
 }
